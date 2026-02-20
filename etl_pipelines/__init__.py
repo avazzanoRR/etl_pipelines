958a0314cb7fd2efc5d39cebd48a3a62a2ca139e
Your Name <you@example.com>
2026-02-20 15:00:49 -0600

    list of sources in init

--- etl_pipelines/__init__.py
+++ etl_pipelines/__init__.py
@@ -1,9 +1,1 @@
-from etl_pipelines.source_1 import connector as source_1_connector
-from etl_pipelines.source_2 import connector as source_2_connector
-from etl_pipelines.source_3 import connector as source_3_connector
-
-SOURCES = [
-    {"name": "source_1", "connector": source_1_connector},
-    {"name": "source_2", "connector": source_2_connector},
-    {"name": "source_3", "connector": source_3_connector},
-]
+SOURCES = ["source_1", "source_2", "source_3"]
