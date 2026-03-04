073366ab1baca02c586bd3d38a5f1ea8b7aaea06
Your Name <you@example.com>
2026-03-04 15:33:01 -0600

    quarantine_file() now creates the quarantine directory if it doesn't exist

--- etl_common/quarantine_file.py
+++ etl_common/quarantine_file.py
@@ -4,6 +4,11 @@ from pathlib import Path
 
 
 def quarantine_file(file_path: str, quarantine_dir: str) -> None:
+    # Create the quarantine directory if it doesn't exist
+    quarantine_dir = Path(quarantine_dir)
+    quarantine_dir.mkdir(parents=True, exist_ok=True)
+
+    # Move the file to the quarantine directory
     src = Path(file_path)
     destination = Path(quarantine_dir) / src.name
     shutil.move(src, destination)
