fc42ea615d2887c543c846045f8a748f7a7248c8
Your Name <you@example.com>
2026-03-09 16:48:44 -0500

    created vimeo data models

---
+++ data_models/vimeo/video_titles.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, Unicode, Identity
+from sqlalchemy.dialects.mssql import DATETIME2
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class DimVideoTitles(Base):
+    __tablename__ = 'DimVideoTitles'
+    __table_args__ = {"schema":"VideoStats.dbo"}
+
+    id = Column(Integer, Identity(start=1,increment=1), primary_key=True)
+    video_uri = Column(Unicode(50))
+    video_title = Column(Unicode(100))
+    video_published_date = Column(DATETIME2)
+    video_program = Column(Unicode(50))
+    source_data_datetime = Column(DATETIME2, nullable=False)
+    record_insert_datetime = Column(DATETIME2, nullable=False)
+    record_update_datetime = Column(DATETIME2, nullable=False)
+
+
+class StageDimVideoTitles(Base):
+    __tablename__ = 'StageDimVideoTitles'
+    __table_args__ = {"schema":"VideoStats.dbo"}
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    video_uri = Column(Unicode(50))
+    video_title = Column(Unicode(100))
+    video_published_date = Column(DATETIME2)
+    video_program = Column(Unicode(50))
+    source_data_datetime = Column(DATETIME2, nullable=False)
+
+
